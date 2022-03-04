ecafe4d9baecb45548c31101f0544cf1fa33599a
turalcavadn <turaltj@code.edu.az>
2022-03-04 18:57:59 +0400

    finished slider section

--- assets/js/owlCarousel.js
+++ assets/js/owlCarousel.js
@@ -1,7 +1,15 @@
+const prevIcon =
+	'<span class="slider-prev-btn product-slider-btn"><i class="fa-solid fa-angle-left"></i></span>';
+const nextIcon =
+	'<span class="slider-next-btn product-slider-btn"><i class="fa-solid fa-angle-right"></i></span>';
+
 $(".owl-carousel").owlCarousel({
 	loop: true,
 	margin: 10,
 	responsiveClass: true,
+	nav: true,
+	navText: [prevIcon, nextIcon],
+	dots: false,
 	responsive: {
 		0: {
 			items: 1,
