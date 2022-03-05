dd79a5a63a5f040a8db286a46bee92283820fc6e
turalcavadn <turaltj@code.edu.az>
2022-03-05 04:54:23 +0400

    featured categories finished

--- assets/js/owlCarousel.js
+++ assets/js/owlCarousel.js
@@ -17,3 +17,32 @@ $(".owl-one").owlCarousel({
 	},
 	items: 1,
 });
+
+const catButtonPrev =
+	'<span class="cat-prev-btn product-slider-btn"><i class="fa-solid fa-angle-left"></i></span>';
+const catButtonNext =
+	'<span class="cat-next-btn product-slider-btn"><i class="fa-solid fa-angle-right"></i></span>';
+
+$(".owl-two").owlCarousel({
+	loop: true,
+	margin: 10,
+	responsiveClass: true,
+	nav: true,
+	navText: [catButtonPrev, catButtonNext],
+	dots: false,
+	responsive: {
+		1400: {
+			items: 10,
+		},
+		1200: {
+			items: 8,
+		},
+		992: {
+			items: 6,
+		},
+		768: {
+			items: 4,
+		},
+	},
+	// items: 10,
+});
