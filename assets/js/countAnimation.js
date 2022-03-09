b21a2f57245c40d9c16b871c36f98e3e425f8e37
turalcavadn <turaltj@code.edu.az>
2022-03-09 09:11:22 +0400

    about finished

---
+++ assets/js/countAnimation.js
@@ -0,0 +1,37 @@
+const count = document.querySelectorAll(".count");
+
+const countSection = document.querySelector(".about-count");
+let done = true;
+
+const counter = () => {
+	windowHeight = window.innerHeight;
+
+	// countSection.getBoundingClientRect().top < windowHeight
+	console.log(windowHeight);
+	console.log(countSection.getBoundingClientRect().top);
+
+	if (countSection.getBoundingClientRect().top < windowHeight - 100 && done) {
+		count.forEach((counter) => {
+			counter.innerText = "0";
+
+			const updateCounter = () => {
+				const target = +counter.getAttribute("data-target");
+				const c = +counter.innerText;
+
+				const increment = target / 100;
+
+				if (c < target) {
+					counter.innerText = `${Math.ceil(c + increment)}`;
+
+					setTimeout(updateCounter, 40);
+				} else {
+					counter.innerText = target;
+				}
+			};
+			updateCounter();
+		});
+		done = false;
+	}
+};
+
+window.addEventListener("scroll", counter);
